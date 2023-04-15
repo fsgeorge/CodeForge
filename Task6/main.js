b33c120d8cba7b7a406be7184664a5e248265b2f
Feba George <84271763+fsgeorge@users.noreply.github.com>
2023-04-15 22:06:23 +0530

    Update main.js

--- Task6/main.js
+++ Task6/main.js
@@ -5,10 +5,10 @@ const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jp
 // Declare an object with alternative text for each image
 const imageAltText = {
   'pic1.jpg': 'Closeup of a blue human eye',
-  'pic2.jpg': 'A yellow flower',
-  'pic3.jpg': 'A red sports car',
-  'pic4.jpg': 'A small, fluffy dog',
-  'pic5.jpg': 'A mountain landscape'
+  'pic2.jpg': 'A mountain landscape',
+  'pic3.jpg': 'White and purple flowers',
+  'pic4.jpg': 'A wall painting',
+  'pic5.jpg': 'A Yellow butterfly'
 };
 
 // Loop through the array of filenames and insert <img> elements into thumb-bar
